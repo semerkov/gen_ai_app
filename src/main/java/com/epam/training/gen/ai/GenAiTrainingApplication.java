3137186ce3c18593087814171b7d0b54f7feb47e
Volodymyr_Semerkov <Volodymyr_Semerkov@epam.com>
2024-11-23 23:59:12 +0100

    Task 1: Generative AI basics

---
+++ src/main/java/com/epam/training/gen/ai/GenAiTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.epam.training.gen.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenAiTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenAiTrainingApplication.class, args);
+	}
+
+}
