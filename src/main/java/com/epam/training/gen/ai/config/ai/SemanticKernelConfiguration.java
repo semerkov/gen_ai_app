87c2a355c8b346a9f8f8e4cb4c3803ef9cf3ce29
Volodymyr_Semerkov <Volodymyr_Semerkov@epam.com>
2024-12-01 00:47:23 +0100

    Task 2: Prompt Engineering

--- src/main/java/com/epam/training/gen/ai/config/ai/SemanticKernelConfiguration.java
+++ src/main/java/com/epam/training/gen/ai/config/ai/SemanticKernelConfiguration.java
@@ -4,8 +4,6 @@ import com.azure.ai.openai.OpenAIAsyncClient;
 import com.epam.training.gen.ai.plugin.SimplePlugin;
 import com.microsoft.semantickernel.Kernel;
 import com.microsoft.semantickernel.aiservices.openai.chatcompletion.OpenAIChatCompletion;
-import com.microsoft.semantickernel.orchestration.InvocationContext;
-import com.microsoft.semantickernel.orchestration.PromptExecutionSettings;
 import com.microsoft.semantickernel.plugin.KernelPlugin;
 import com.microsoft.semantickernel.plugin.KernelPluginFactory;
 import com.microsoft.semantickernel.services.chatcompletion.ChatCompletionService;
@@ -16,8 +14,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
-
-import java.util.Map;
 
 /**
  * Configuration class for setting up Semantic Kernel components.
@@ -76,44 +72,10 @@ public class SemanticKernelConfiguration {
                 .build();
     }
 
-    /**
-     * Creates an {@link InvocationContext} bean with default prompt execution settings.
-     *
-     * @return an instance of {@link InvocationContext}
-     */
-    @Bean
-    public InvocationContext invocationContext(@Value("${client-sk-temperature:1.0}") double temperature,
-            @Value("${client-max-tokens:256}") int maxTokens) {
-
-        return InvocationContext.builder()
-                .withPromptExecutionSettings(PromptExecutionSettings.builder()
-                        .withTemperature(temperature)
-                        .withMaxTokens(maxTokens)
-                        .build())
-                .build();
-    }
-
-    /**
-     * Creates a map of {@link PromptExecutionSettings} for different models.
-     *
-     * @param deploymentOrModelName the Azure OpenAI deployment or model name
-     * @return a map of model names to {@link PromptExecutionSettings}
-     */
-    @Bean
-    public Map<String, PromptExecutionSettings> promptExecutionsSettingsMap(
-            @Value("${client-openai-deployment-name}") String deploymentOrModelName,
-            @Value("${client-sk-temperature:1.0}") double temperature,
-            @Value("${client-max-tokens:256}") int maxTokens) {
-
-        return Map.of(deploymentOrModelName, PromptExecutionSettings.builder()
-                .withTemperature(temperature)
-                .withMaxTokens(maxTokens)
-                .build());
-    }
-
     @Bean
     @Scope(scopeName = "session", proxyMode = ScopedProxyMode.TARGET_CLASS)
     public ChatHistory chatHistory() {
+
         return new ChatHistory();
     }
 }
