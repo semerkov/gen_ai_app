ffe738d7dada82ece3cdbba704cb0a472e6cfd36
Volodymyr_Semerkov <Volodymyr_Semerkov@epam.com>
2024-12-08 00:46:48 +0100

    Task 3: Work with Different Models

--- src/main/java/com/epam/training/gen/ai/service/SemanticKernelService.java
+++ src/main/java/com/epam/training/gen/ai/service/SemanticKernelService.java
@@ -1,18 +1,26 @@
 package com.epam.training.gen.ai.service;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.azure.ai.openai.OpenAIAsyncClient;
 import com.microsoft.semantickernel.Kernel;
+import com.microsoft.semantickernel.aiservices.openai.chatcompletion.OpenAIChatCompletion;
 import com.microsoft.semantickernel.orchestration.FunctionResult;
 import com.microsoft.semantickernel.orchestration.InvocationContext;
 import com.microsoft.semantickernel.orchestration.InvocationReturnMode;
 import com.microsoft.semantickernel.orchestration.PromptExecutionSettings;
 import com.microsoft.semantickernel.orchestration.ToolCallBehavior;
+import com.microsoft.semantickernel.plugin.KernelPlugin;
 import com.microsoft.semantickernel.semanticfunctions.KernelFunction;
 import com.microsoft.semantickernel.semanticfunctions.KernelFunctionArguments;
+import com.microsoft.semantickernel.services.chatcompletion.ChatCompletionService;
 import com.microsoft.semantickernel.services.chatcompletion.ChatHistory;
 
 import lombok.extern.slf4j.Slf4j;
@@ -21,37 +29,53 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class SemanticKernelService {
 
+    private static final String SIMPLE_KERNEL_PREFIX = "simple_";
+    private static final String CURRENCY_EXCHANGE_KERNEL_PREFIX = "currencyExchange_";
+
+    @Value("${client-openai-deployment-name}")
+    private String defaultDeploymentName;
+
     @Value("${client-sk-temperature:1.0}")
     private double defaultTemperature;
 
     @Value("${client-max-tokens:256}")
     private int defaultMaxTokens;
 
-    private final Kernel semanticKernel;
-    private final Kernel currencyExchangeRateKernel;
+    private OpenAIAsyncClient openAIAsyncClient;
+    private KernelPlugin kernelPlugin;
+    private KernelPlugin currencyExchangeRateKernelPlugin;
     private final ChatHistory chatHistory;
+    private final Map<String, Kernel> kernelMap = new ConcurrentHashMap<>();
 
     @Autowired
-    public SemanticKernelService(Kernel semanticKernel, Kernel currencyExchangeRateKernel, ChatHistory chatHistory) {
+    public SemanticKernelService(OpenAIAsyncClient openAIAsyncClient, KernelPlugin currencyExchangeRateKernelPlugin,
+            KernelPlugin kernelPlugin, ChatHistory chatHistory) {
 
-        this.semanticKernel = semanticKernel;
-        this.currencyExchangeRateKernel = currencyExchangeRateKernel;
+        this.openAIAsyncClient = openAIAsyncClient;
+        this.kernelPlugin = kernelPlugin;
+        this.currencyExchangeRateKernelPlugin = currencyExchangeRateKernelPlugin;
         this.chatHistory = chatHistory;
     }
 
-    public String processWithHistory(String input, Double temperature, Integer maxTokens) {
+    public String processWithHistory(String input, String deploymentName, Double temperature, Integer maxTokens) {
 
-        return processOnKernelWithHistory(semanticKernel, input, temperature, maxTokens);
+        return processOnKernelWithHistory(kernelPlugin, input, deploymentName, temperature, maxTokens);
     }
 
-    public String getCurrencyExchangeRate(String input, Double temperature, Integer maxTokens) {
+    public String getCurrencyExchangeRate(String input, String deploymentName, Double temperature, Integer maxTokens) {
 
-        return processOnKernelWithHistory(currencyExchangeRateKernel, input, temperature, maxTokens);
+        return processOnKernelWithHistory(currencyExchangeRateKernelPlugin, input, deploymentName, temperature,
+                maxTokens);
     }
 
-    private String processOnKernelWithHistory(Kernel kernel, String input, Double temperature, Integer maxTokens) {
+    private String processOnKernelWithHistory(KernelPlugin kernelPlugin, String kernelPrefix, String input, String deploymentName,
+            Double temperature, Integer maxTokens) {
 
         InvocationContext invocationContext = buildInvocationContext(temperature, maxTokens);
+
+        deploymentName = StringUtils.defaultIfBlank(deploymentName, defaultDeploymentName);
+        Kernel kernel = getKernel(deploymentName, kernelPlugin, kernelPrefix);
+
         FunctionResult<String> response = kernel.invokeAsync(getChat())
                 .withArguments(getKernelFunctionArguments(input, chatHistory))
                 .withInvocationContext(invocationContext)
@@ -79,6 +103,21 @@ public class SemanticKernelService {
                         {{$chatHistory}}
                         <message role="user">{{$input}}</message>""")
                 .build();
+    }
+
+    private Kernel getKernel(String deploymentName, KernelPlugin kernelPlugin, String kernelPrefix) {
+
+        return kernelMap.computeIfAbsent(deploymentName, (v) -> {
+            ChatCompletionService chatCompletionService = OpenAIChatCompletion.builder()
+                    .withModelId(deploymentName)
+                    .withOpenAIAsyncClient(openAIAsyncClient)
+                    .build();
+
+            return Kernel.builder()
+                    .withAIService(ChatCompletionService.class, chatCompletionService)
+                    .withPlugin(kernelPlugin)
+                    .build();
+        });
     }
 
 
